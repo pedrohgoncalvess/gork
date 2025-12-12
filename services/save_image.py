2cdb5c1d926350b7147b3a49a04c93d3fedf2386
Pedro H. <pedro_gonsalves@hotmail.com>
2025-12-12 10:31:39 -0300

    refac: call generate embeddings function

--- services/save_image.py
+++ services/save_image.py
@@ -1,34 +1,32 @@
-import base64
-import io
 from datetime import datetime
 from typing import Optional
 from uuid import uuid4
-
-from PIL import Image
-from transformers import AutoProcessor, AutoModel
-import torch
+import base64
+import io
 
 from database import PgConnection
 from database.models.base import Group, User
 from database.models.content import Media, Message
 from database.operations.base import GroupRepository, UserRepository
 from database.operations.content import MediaRepository, MessageRepository
+from embeddings import generate_image_embeddings, generate_text_embeddings
 from s3 import S3Client
 from services import translate_to_pt
 
-
-import base64
-import io
-
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
+
+
+# from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+# processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+# model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+
+processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b")
 
 def generate_title_image(base64_bytes: bytes) -> str:
     image = Image.open(io.BytesIO(base64.b64decode(base64_bytes)))
-
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
     inputs = processor(image, return_tensors="pt")
     output = model.generate(**inputs, max_new_tokens=20)
@@ -36,11 +34,6 @@ def generate_title_image(base64_bytes: bytes) -> str:
     caption = processor.decode(output[0], skip_special_tokens=True)
     return caption
 
-model_name = "google/siglip-base-patch16-224"
-
-processor = AutoProcessor.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
-model.eval()
 
 async def materialize_image(message_id: str, image_base64: bytes, group_id: Optional[int] = None, user_id: Optional[int] = None) -> Media:
     s3_conn = S3Client()
@@ -56,29 +49,11 @@ async def materialize_image(message_id: str, image_base64: bytes, group_id: Opti
             ext_id = user.ext_id
 
         decoded = base64.b64decode(image_base64)
-
-        image = Image.open(io.BytesIO(decoded)).convert("RGB")
-        inputs = processor(images=image, return_tensors="pt")
-
         name = generate_title_image(image_base64)
         translated_name = translate_to_pt(name)
 
-        inputs_text = processor(
-            text=[translated_name],
-            return_tensors="pt",
-            padding=True,
-            truncation=True
-        ).to("cpu")
-
-        with torch.no_grad():
-            emb = model.get_image_features(**inputs)
-            text_emb = model.get_text_features(**inputs_text)
-
-        emb = emb / emb.norm(p=2, dim=-1, keepdim=True)
-        emb = emb.squeeze(0).cpu().numpy()
-
-        text_emb = text_emb / text_emb.norm(p=2, dim=-1, keepdim=True)
-        text_emb = text_emb.squeeze().cpu().numpy()
+        emb = await generate_image_embeddings(decoded)
+        text_emb = await generate_text_embeddings(translated_name)
 
         image_id = uuid4()
         path = f"{ext_id}/{datetime.now().strftime("%Y-%m-%d")}/{image_id}.png"
@@ -94,8 +69,8 @@ async def materialize_image(message_id: str, image_base64: bytes, group_id: Opti
             Media(
                 ext_id=image_id, name=translated_name, message_id=message.id,
                 bucket="whatsapp", path=path, format="png",
-                size=len(decoded) / float((1024 * 1024)), image_embedding=emb.tolist(),
-                name_embedding=text_emb.tolist()
+                size=len(decoded) / float((1024 * 1024)), image_embedding=emb,
+                name_embedding=text_emb
             )
         )
 
