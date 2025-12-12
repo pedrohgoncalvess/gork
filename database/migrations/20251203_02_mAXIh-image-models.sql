889902a4586c211beafd266c127f0260d6ac9ac4
Pedro H. <pedro_gonsalves@hotmail.com>
2025-12-12 10:34:38 -0300

    add: new image models

--- database/migrations/20251203_02_mAXIh-image-models.sql
+++ database/migrations/20251203_02_mAXIh-image-models.sql
@@ -6,5 +6,6 @@ ALTER TABLE "manager"."model" ADD COLUMN audio_default BOOLEAN DEFAULT FALSE;
 ALTER TABLE "manager"."model" RENAME COLUMN "default" TO text_default;
 ALTER TABLE "manager"."model" DROP COLUMN "audio_input";
 INSERT INTO "manager"."model" (name, openrouter_id, input_price, output_price, image_default) VALUES
-('Gemini 2.5 Flash Image (Nano Banana)', 'google/gemini-2.5-flash-image', 0.30, 2.5, true);
-UPDATE "manager"."model" SET text_default = false, audio_default = true WHERE openrouter_id = 'google/gemini-2.0-flash-lite-001';
+('Gemini 2.5 Flash Image (Nano Banana)', 'google/gemini-2.5-flash-image', 0.30, 20, true),
+('Black Forest Labs: FLUX.2 Pro', 'black-forest-labs/flux.2-pro', 3.66, 3.66, false),
+('OpenAI: GPT-5 Image Mini', 'openai/gpt-5-image-mini', 2.5, 8, false);
