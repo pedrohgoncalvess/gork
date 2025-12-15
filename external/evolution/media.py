d9ede88a07f8c1771bbf611ec362698252ebf242
Pedro H. <pedro_gonsalves@hotmail.com>
2025-12-14 21:57:38 -0300

    add: return file name too

--- external/evolution/media.py
+++ external/evolution/media.py
@@ -3,7 +3,7 @@ import httpx
 from external.evolution.base import evolution_instance_name, evolution_api_key, evolution_api
 
 
-async def download_media(message_id: str) -> bytes:
+async def download_media(message_id: str) -> tuple[bytes, str]:
     media_url = f"{evolution_api}/chat/getBase64FromMediaMessage/{evolution_instance_name}"
 
     payload = {
@@ -27,4 +27,4 @@ async def download_media(message_id: str) -> bytes:
         result = response.json()
 
         if 'base64' in result:
-            return result['base64']
+            return result["base64"], result["fileName"]
