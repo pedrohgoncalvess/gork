844586de6911dd2b8d525c9b43bfc67957729685
Pedro H. <pedro_gonsalves@hotmail.com>
2026-05-07 10:44:33 -0300

    feat: add manager embedding and model override schema

--- database/migrations/20251221_02_lKJ59-big-refactor.rollback.sql
+++ database/migrations/20251221_02_lKJ59-big-refactor.rollback.sql
@@ -1,4 +1,4 @@
--- expensive control
+-- big refactor
 -- depends: 20251221_01_7rSRX-favorite-message
 
 ALTER TABLE "manager"."agent" DROP COLUMN "model_id" INTEGER;
--- database/operations/manager/__init__.py
+++ database/operations/manager/__init__.py
@@ -1,5 +1,7 @@
 from database.operations.manager.agent import AgentRepository
 from database.operations.manager.command import CommandRepository
+from database.operations.manager.embedding import EmbeddingRepository
 from database.operations.manager.interaction import InteractionRepository
 from database.operations.manager.model import ModelRepository
+from database.operations.manager.model_conversation import ModelConversationRepository
 from database.operations.manager.remember import RememberRepository
