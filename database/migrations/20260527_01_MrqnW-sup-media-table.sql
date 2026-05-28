2997ffee6761020de54379d05636c7ad7060485d
Pedro H. <pedro_gonsalves@outlook.com.br>
2026-05-28 11:50:13 -0300

    feat: add sup_media table and models

---
+++ database/migrations/20260527_01_MrqnW-sup-media-table.sql
@@ -0,0 +1,13 @@
+-- sup media table
+-- depends: 20251221_03_i2N4b-fix-message-media-fk
+
+CREATE TABLE "content"."sup_media" (
+    id SERIAL,
+    name VARCHAR(50) NOT NULL,
+    bucket VARCHAR(50) NOT NULL,
+    path VARCHAR(150) NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    inserted_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'America/Sao_Paulo'),
+
+    CONSTRAINT sup_media_pk PRIMARY KEY (id)
+);
